f48c3374179b415786268b8d955a2e90fe6f28bb
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-16 17:46:20 +0530

    Update and rename info to  Notes.h

---
+++ Notes.h
@@ -0,0 +1,11 @@
+code file.py   -> creates a new python file (Type in terminal)
+python file.py -> To get Output
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.responses.create(
+    input="In one sentence, what is CS50?",
+    model="gpt-5"
+)
+print(response.output_text)
