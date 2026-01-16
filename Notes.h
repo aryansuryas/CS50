2c6dccac87a4128e450cd35d594ab021c89e0067
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-16 23:51:59 +0530

    Update Notes.h

--- Notes.h
+++ Notes.h
@@ -69,7 +69,7 @@ print(response.output_text)
 
   Dec	Chr		Dec	Chr		  Dec	Chr	      	Dec	Chr	     	Dec	Chr	       	Dec	Chr		    Dec	Chr		     Dec	Chr
   0 -	NUL		16 -	DLE		32 -	SP		48 -	0		64 -	@		80 -	P		96 -	`		112 -	p
-  1 -	SOH		17 -	DC1		33 -	!		49 -	1		65 -	A		81 -	Q		97 -	a		113 -	q
+  1 -	SOH		17 -	DC1		33 -	!		49 -	1	  🔴65 -	A		81 -	Q	  🔴97 -	a		113 -	q
   2 -	STX		18 -	DC2		34 -	”		50 -	2		66 -	B		82 -	R		98 -	b		114 -	r
   3 -	ETX		19 -	DC3		35 -	#		51 -	3		67 -	C		83 -	S		99 -	c		115 -	s
   4 -	EOT		20 -	DC4		36 -	$		52 -	4		68 -	D		84 -	T		100 -	d		116 -	t
@@ -78,7 +78,7 @@ print(response.output_text)
   7 -	BEL		23 -	ETB		39 -	’		55 -	7		71 -	G		87 -	W		103 -	g		119 -	w
   8 -	BS		24 -	CAN		40 -	(		56 -	8		72 -	H		88 -	X		104 -	h		120 -	x
   9 -	HT		25 -	EM		41 -	)		57 -	9		73 -	I		89 -	Y		105 -	i		121 -	y
-  10 -	LF	    26 -	SUB		42 -	*		58 -	:	    74 -	J		90 -	Z		106 -	j		122 -	z
+  10 -	LF	    26 -	SUB		42 -	*		58 -	:	    74 -	J	  🟢90 -	Z		106 -	j	  🟢122 -	z
   11 -	VT		27 -	ESC		43 -	+		59 -	;	    75 -	K		91 -	[		107 -	k		123 -	{
   12 -	FF		28 -	FS		44 -	,		60 -	<	    76 -	L		92 -	\		108 -	l		124 -	|
   13 -	CR		29 -	GS		45 -	-		61 -	=	    77 -	M		93 -	]		109 -	m		125 -	}
