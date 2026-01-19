39d6e631026abfb5f16a7201b41a2947c6996f1b
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-19 23:38:26 +0530

    Update Notes.h

--- Notes.h
+++ Notes.h
@@ -1,4 +1,4 @@
-                                                              Introducton TO Harvard University - CS50 edX
+                                                          Introducton TO Harvard University - CS50 edX
 
                                                                                  
 code file.py   -> creates a new python file (Type in terminal)
@@ -230,3 +230,28 @@ Music can be represented similarly using various combinations of bytes.
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Algorithm :: Step by Step Instructions To Solve a Problem..
+Pseudocode
+Pseudocode is human-readable instructions that often describe the steps of an algorithm.
+The ability to create pseudocode is central to one’s success in both this class and in computer programming.
+For example, considering the third algorithm above, we could compose pseudocode as follows:
+
+1  Pick up phone book
+2  Open to middle of phone book
+3  Look at page
+4  If person is on page
+5      Call person
+6  Else if person is earlier in book
+7      Open to middle of left half of book
+8      Go back to line 3
+9  Else if person is later in book
+10     Open to middle of right half of book
+11     Go back to line 3
+12 Else
+13     Quit
+Pseudocoding is such an important skill for at least two reasons. First, when you pseudocode before you create formal code, it allows you to think through the logic of your problem in advance. Second, when you pseudocode, you can later provide this information to others that are seeking to understand your coding decisions and how your code works.
+Notice that the language within our pseudocode has some unique features. First, some of these lines begin with verbs like pick up, open, look at. Later, we will call these functions.
+Second, notice that some lines include statements like if or else if. These are called conditionals.
+Third, notice how there are expressions that can be stated as true or false, such as “person is earlier in the book.” We call these boolean expressions.
+Finally, notice how there are statements like “go back to line 3.” We call these loops.
+These building blocks are the fundamentals of programming.
+In the context of Scratch, which is discussed below, we will use each of the above basic building blocks of programming.
